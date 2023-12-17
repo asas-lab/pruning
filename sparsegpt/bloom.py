0000ecc7d3030ad5d22df542b30094598a6d7775
Emad Alghamdi <37039039+Eaalghamdi@users.noreply.github.com>
2023-12-17 20:59:30 +0300

    Update bloom.py

--- sparsegpt/bloom.py
+++ sparsegpt/bloom.py
@@ -216,7 +216,7 @@ if __name__ == '__main__':
         help='BLOOM model to load; pass `bigscience/bloom-X`.'
     )
     parser.add_argument(
-        'dataset', type=str, choices=['wikitext2', 'ptb', 'c4'],
+        'dataset', type=str, choices=['wikitext2', 'ptb', 'c4', 'arabic_wikipedia'],
         help='Where to extract calibration data from.'
     )
     parser.add_argument(
